92ebb37135873aba85ebb84b2ea778569a756ecd
Ariful <arifulmit17@gmail.com>
2026-04-30 14:22:32 +0600

    fised intro image spacing issue

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -25,7 +25,7 @@ const Intro = () => {
 
   {/* Image */}
   <motion.div
-    className="w-full lg:w-1/2 flex justify-center lg:justify-start"
+    className="w-full lg:w-1/2 flex justify-center lg:justify-center"
     initial={{ scale: 0.8, opacity: 0 }}
     whileInView={{ scale: 1, opacity: 1 }}
     transition={{ duration: 0.6, delay: 0.2 }}
